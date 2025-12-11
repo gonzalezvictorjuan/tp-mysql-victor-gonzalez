a3fca14eea46eae16336adee829af0020bcfcbd0
Victor Juan González <g.victor.juan@gmail.com>
2025-12-11 19:38:57 -0300

    resolucion del tp

---
+++ tp.sql
@@ -0,0 +1,6 @@
+-- Ejercicio 1 – Crear Base de Datos
+-- Crear una base de datos llamada veterinaria_patitas_felices.
+create asdasdasd
+-- Ejercicio 2 – Crear tabla duenos
+-- Crear la tabla duenos con las siguientes columnas:
+--- ....
